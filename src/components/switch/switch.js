dbb644b38a7bd89d96831d0e9b01adaeebe4bb84
Harurow <Harurow@users.noreply.github.com>
2022-05-04 00:11:42 +0900

    ♻️ 記事に合わせてリファクタリング・コメント追加

--- src/components/switch/switch.js
+++ src/components/switch/switch.js
@@ -5,97 +5,58 @@
  */
 class MySwitch extends HTMLElement {
   /**
-   * 要素が生成されました
+   * コンポーネント作成
    */
   constructor () {
     super()
+
+    // shadow domにアタッチ
     this.shadow = this.attachShadow({ mode: 'open' })
   }
 
   /**
-   * ブラウザは要素が document に追加された時にこれを呼びます
-   * (要素が繰り返し追加/削除される場合、何度も呼ばれます)
+   * コンポーネントが document に追加したタイミングで呼び出される。
+   * 追加、削除を繰り返すとその度に呼び出される。
    */
   connectedCallback () {
     this.render()
   }
 
   /**
-   * 監視対象の属性名の配列
+   * コンポーネントが document から削除されたときに呼び出されます。
+   * 追加、削除を繰り返すとその度に呼び出される。
    */
-  static observedtAttributesTargets = ['checked', 'value', 'disabled', 'indeterminate']
-
-  /**
-   * 変更を監視する属性名の配列を取得する
-   */
-  static get observedAttributes () {
-    return MySwitch.observedtAttributesTargets
+  disconnectedCallback () {
   }
 
   /**
-   * observedAttributesで挙げたいずれかの属性が変更されたときに呼ばれま
+   * 監視対象のタグ名を配列で返す
+   */
+  static get observedAttributes () {
+    return ['checked', 'value', 'disabled', 'indeterminate']
+  }
+
+  /**
+   * observedAttributes で列挙した属性が変更されたときに呼び出される
    * @param {string} name
    * @param {any} oldValue
    * @param {any} newValue
    */
   attributeChangedCallback (name, oldValue, newValue) {
-    if (MySwitch.observedtAttributesTargets.includes(name)) {
-      this.render()
-    }
+    // 監視属性の変更に合わせて再レンダリング
+    this.render()
   }
 
-  get value () {
-    return this.getAttribute('value') ?? 'on'
-  }
-
-   set value (v) {
-    this.setAttribute('value', v)
-  }
-
-  get indeterminate () {
-    return this.hasAttribute('indeterminate')
-  }
-
-  set indeterminate (v) {
-    const b = Boolean(v)
-    if (b) {
-      this.setAttribute('indeterminate', undefined)
-    } else {
-      this.removeAttribute('indeterminate')
-    }
-  }
-
-  get checked () {
-    return this.hasAttribute('checked')
-  }
-
-  set checked (v) {
-    const b = Boolean(v)
-    if (b) {
-      this.setAttribute('checked', undefined)
-    } else {
-      this.removeAttribute('checked')
-    }
-  }
-
-  get disabled () {
-    return this.hasAttribute('disabled')
-  }
-
-  set disabled (v) {
-    const b = Boolean(v)
-    if (b) {
-      this.setAttribute('disabled', undefined)
-    } else {
-      this.removeAttribute('disabled')
-    }
+  /**
+   * コンポーネントが新しい document に移動 (adoptNode)した場合に呼び出される
+   */
+  adoptedCallback() {
   }
 
   /**
    * レンダリング
    */
   render () {
-    const $ = (s) => document.querySelector(s)
     const checked = this.checked
     const disabled = this.disabled
 
@@ -125,6 +86,81 @@ class MySwitch extends HTMLElement {
     checkbox.addEventListener('change', this.fireChangeEvent)
   }
 
+  /**
+   * value属性の取得
+   */
+  get value () {
+    return this.getAttribute('value') ?? 'on'
+  }
+
+  /**
+   * value属性の設定
+   */
+  set value (v) {
+    this.setAttribute('value', v)
+  }
+
+  /**
+   * indeterminate属性の取得
+   */
+  get indeterminate () {
+    return this.hasAttribute('indeterminate')
+  }
+
+  /**
+   * indeterminate属性の設定
+   */
+  set indeterminate (v) {
+    const b = Boolean(v)
+    if (b) {
+      this.setAttribute('indeterminate', undefined)
+    } else {
+      this.removeAttribute('indeterminate')
+    }
+  }
+
+  /**
+   * checked属性の取得
+   */
+  get checked () {
+    return this.hasAttribute('checked')
+  }
+
+  /**
+   * checked属性の設定
+   */
+  set checked (v) {
+    const b = Boolean(v)
+    if (b) {
+      this.setAttribute('checked', undefined)
+    } else {
+      this.removeAttribute('checked')
+    }
+  }
+
+  /**
+   * disabled属性の取得
+   */
+  get disabled () {
+    return this.hasAttribute('disabled')
+  }
+
+  /**
+   * disabled属性の設定
+   */
+  set disabled (v) {
+    const b = Boolean(v)
+    if (b) {
+      this.setAttribute('disabled', undefined)
+    } else {
+      this.removeAttribute('disabled')
+    }
+  }
+
+  /**
+   * changeイベントの発行
+   * @param {Event} e
+   */
   fireChangeEvent(e) {
     if (e.composed === false) {
       const event = new CustomEvent('change', {
